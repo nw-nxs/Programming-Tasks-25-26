3bdbf17a3f4cd87e97e3488375b1ddc3797cc6fd
NWxNEXUS <47587@rws.uk.net>
2026-09-14 11:41:15 +0100

    Update 01_Grade_Calculation.py

--- Week_02_GCSE_Basics/01_Grade_Calculation.py
+++ Week_02_GCSE_Basics/01_Grade_Calculation.py
@@ -14,12 +14,14 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
-
-
-if __name__ == "__main__":
-    main()
+score = [A,B,C,D]
+def get_grade(score):
+    student = int(input("Enter score: "))
+    if student >= 80 and student <= 100:
+        print("Your grade is A")
+    elif student >= 60 and student <= 79:
+        print("Your grade is B")
+    elif student >= 40 and student <= 59:
+        print("Your grade is C")
+    elif student < 40:
+        print("Your grade is D")
