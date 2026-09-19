04359e92a7ff6e5ca40c741b6612e94c484127a4
NWxNEXUS <47587@rws.uk.net>
2026-09-20 00:01:42 +0100

    Update 04_Password_Strength.py

--- Week_02_GCSE_Basics/04_Password_Strength.py
+++ Week_02_GCSE_Basics/04_Password_Strength.py
@@ -13,13 +13,37 @@ TODO:
 - Fill in functions
 - Add demonstration code under `if __name__ == "__main__":`
 """
+password = str(input("Enter a password: "))
+strength = 0
+num = str(1234567890)
+lower = str(abcdefghijklmnopqrstuvwxyz)
+cap = str(ABCDEFGHIJKLMNOPQRSTUVWXYZ)
+if len(password) == 8:
+    strength += 1
+for i in range(len(num)):
+    if num[i] in password:
+        strength += 1
+for i in range(len(lower)):
+    if lower[i] in password:
+        strength += 1
+for i in range(len(cap)):
+    if cap[i] in password:
+        strength += 1
+if strength == 0 or strength == 1:
+    print("password is weak")
+if strength == 2:
+    print("password is medium")
+if strength == 3:
+    print("password is strong")
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
 
 
-if __name__ == "__main__":
-    main()
+
+
+
+
+
+
+
+
+
